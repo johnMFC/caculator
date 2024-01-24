03680b58d6ea48089bfcf8b8d9fd8bb00dee9fb7
Johnatas michaell <johnatasmichael067@gmail.com>
2024-01-24 16:04:12 -0300

    memory_calculator

--- cl/projeto_cl/lib/screens/calculator.dart
+++ cl/projeto_cl/lib/screens/calculator.dart
@@ -1,5 +1,6 @@
 import 'package:calculadora/models/memory.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../components/display.dart';
 import '../components/keyboard.dart';
 
@@ -18,6 +19,9 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
     return MaterialApp(
     home: Column(
       children: <Widget>[
